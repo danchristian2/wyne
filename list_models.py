eb01896e39a0a5db4e4892617bcd91190f30a973
Dan Christian <danchristianniyomugabo@gmail.com>
2026-08-29 22:32:57 +0200

    small change

--- list_models.py
+++ list_models.py
@@ -5,5 +5,8 @@ from google import genai
 load_dotenv()
 client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
 
+model_number = 0
 for model in client.models.list():
     print(model.name, "-", model.supported_actions)
+    model_number += 1
+print(f"The number of the models is {model_number}")
